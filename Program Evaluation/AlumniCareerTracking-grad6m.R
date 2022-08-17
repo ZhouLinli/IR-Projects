48445f31cbf3461a24cac8d6b3b38c98c3c3115f
Linli Zhou <linli.zhou@ucla.edu>
2022-08-16 17:30:39 -0700

    read and select 17-21 ipeds.complete pcid,degree,graddate,major,race,gender,level

--- Program Evaluation/AlumniCareerTracking-grad6m.R
+++ Program Evaluation/AlumniCareerTracking-grad6m.R
@@ -50,40 +50,12 @@ names(survey)[names(survey)%in%names(db)=="FALSE"]
 #############matching survey degree info in ipeds.complete#######################
 #which year of ipeds.complete do we need:
 survey%>%group_by(GradYear)%>%count()#it is 20-21 graduates
-#try 2021 fall ipeds.complete
-#check which sheet to read
-excel_sheets("/Users/linlizhou/Documents/LASELL/data/completion/2021IPEDScompletions.xlsx")
-#read data
-ipeds.complete21f<-read_excel("/Users/linlizhou/Documents/LASELL/data/completion/2021IPEDScompletions.xlsx",sheet = "Degrees - Combined")
-#check degree codes
-ipeds.complete21f%>%group_by(DEGREE)%>%count()#all degree codes seem fine
-# - if find all pcid in survey for degree
+# load ipeds.complete data
+
+# check if find all pcid in survey for degree
 survey$PC_ID %in% ipeds.complete21f$textbox41#partially true
 #investigate those not true
 survey[survey$PC_ID %in% ipeds.complete21f$textbox41=="FALSE",c(3,4)]#maybe they're from 2020 graduate data, then needs to find ipeds.complete20f
-
-#try 2020 fall ipeds.complete
-#find the right sheet to read
-excel_sheets("/Users/linlizhou/Documents/LASELL/data/completion/2020Merged Completions.xlsx")
-#read data
-ipeds.complete20f<-read_excel("/Users/linlizhou/Documents/LASELL/data/completion/2020Merged Completions.xlsx",sheet = "Merged")
-#check degree codes
-ipeds.complete20f%>%group_by(Degree)%>%count()#all degree codes seem fine
-# if find pcid in survey for degree
-survey$PC_ID %in% ipeds.complete20f$`People Code ID`#only two true
-#then there might be students who did not receive degree (not in the complete dataset, but is in the survey)
-
-#anyway,merge ipeds.complete20/21f
-#select useful cols
-ipeds.complete20f<-ipeds.complete20f%>%select(`People Code ID`,Degree)
-ipeds.complete21f<-ipeds.complete21f%>%select(textbox41,DEGREE)
-#want to merge col to col exactly append one to the other
-#then need to have exact same names:
-names(ipeds.complete21f)<-names(ipeds.complete20f)
-#merge
-ipeds.complete<-full_join(ipeds.complete20f,ipeds.complete21f)
-#remove used datasets
-rm(ipeds.complete20f,ipeds.complete21f)
 
 #can vlookup now by merging ipeds.complete with survey, but only those have a match in survey
 survey<-left_join(survey,ipeds.complete,by=c("PC_ID"="People Code ID"))

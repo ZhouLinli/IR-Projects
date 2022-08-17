00a5f643745c3b2e5d607821bd46fabea08b25c4
Linli Zhou <linli.zhou@ucla.edu>
2022-08-16 18:39:22 -0700

    rename linkedin using survey names-disregard this file

--- Program Evaluation/AlumniCareerTracking-gd6m.R
+++ Program Evaluation/AlumniCareerTracking-gd6m.R
@@ -1,4 +1,5 @@
 #This file is a simplified version of "AlumniCareerTracking-grad6m.R". It merges only linkedin and survey data (i.e., disregarding historical data).
+
 #############################load package#############################
 library(readxl)
 library(dplyr)
@@ -9,17 +10,18 @@ survey<-read_excel("/Users/linlizhou/Documents/LASELL/data/alumni/Grad 6-month s
 
 ###########################################################################
 ######################survey data: to match db cols########################
-######remove auto-generated survey cols (timestart, status, etc.)#######
+######remove auto-generated survey cols (timestart, status, etc.)
 #find out the index
 names(survey)
+#remove auto-generated survey cols (timestart, status, etc.)
 survey<-survey%>%select(c(`First Name`,`Last Name`,`Invite Custom Field 1`,53:94))%>%rename(PCID="Invite Custom Field 1")
 
-#############matching survey degree info in pop#######################
+#############matching survey degree info in pop
 #read data
 pop<-read_excel("/Users/linlizhou/Documents/LASELL/data/alumni/Class of 2021 GR Population.xlsx")
 #select useful cols
 pop<-pop%>%select(`Power Campus ID`,`Degree: Alumni Degree Name`)%>%rename(PCID="Power Campus ID")
-#merge
+#merge to have degree name
 survey<-left_join(survey,pop)
 #name of survey
 names(survey)

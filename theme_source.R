71cff4242bfe4ac16177522cce5f842f07a544b1
Linli Zhou <linli.zhou@ucla.edu>
2022-10-20 19:33:45 -0500

    edited theme_lz() without seting plot.margin, set it within the rmd ffplot itself

--- theme_source.R
+++ theme_source.R
@@ -4,6 +4,11 @@ library(tidyverse)
 library(formattable)
 library(stringr)
 library(ggrepel)
+library(writexl)
+library(waffle)
+library(patchwork)
+library(wordcloud)
+library(broom)
 
 mycolors<-c("#13294b","#5c88da","#69b3e7","#da291c","#641F45","#fcb53b","#6C6F70")#1-3blue,4-5red,6yellow,7grey
 
@@ -21,23 +26,31 @@ theme_minimal() %+replace%    #replace elements already strips axis lines,
     strip.background = element_blank()#facet background
   )}
 
+#font_import("/Users/linlizhou/Library/Fonts/Georgia.ttf")
+#library(extrafont)
+#extrafont::loadfonts()
 theme_lz <- function(){ 
   font <- "Helvetica"   #assign font family up front
   theme_minimal() %+replace%    #replace elements already strips axis lines, 
     theme(
+      #plot.margin = margin(t = 20, r = 10, b = 40,l = 10,unit = "pt"), 
+      #plot.margin = margin(2,0,2,0,"cm"),
       panel.grid.major = element_blank(),    #no major gridlines
       panel.grid.minor = element_blank(),    #no minor gridlines
-      plot.title = element_text(family = font, size = 9, face = 'bold',hjust = 0.5, vjust = 2),
+      plot.title = element_text(family = font, size = 8, face = 'bold',hjust = 0, vjust = 0),#hjust=0 left, vjust=-1 close to graph (move to the bottom)
+      plot.subtitle=element_text(size=8, hjust=0.5, face="italic", color="black"),
       axis.title = element_text(family = font, size = 9), 
       axis.text = element_text(family = font, size = 9), 
-      axis.text.x = element_text(family = font, size = 9, margin = margin(t=5,b=10)),
+      axis.text.x = element_blank(),#element_text(family = font, size = 9, margin = margin(t=5,b=10)),
       axis.ticks = element_blank(),          #strip axis ticks
       axis.text.y=element_blank(),
       legend.title = element_text(family = font, size=9),
+      legend.margin=margin(t=-25),
       legend.text = element_text(family = font, size=7),
       legend.position="bottom",
       legend.direction = "horizontal",
       strip.text = element_text(family = font, size = 9, margin = margin(t=5,b=10)),#facet label text, move up is + 1
+      #strip.position = "bottom",
       strip.background = element_blank()#facet background
     )}
 
